a36c39d6ec981eb9fcdd0a6b689c46e74cde5578
Cynthia Wasonga <wasonga.cynthia@gmail.com>
2019-01-01 13:15:54 +0300

    Create script to mark wether logs are quoted or not

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import MySQLdb
+import MySQLdb.cursors as cursors
 
 
 class DatabaseConnection(object):
@@ -10,7 +11,8 @@ class DatabaseConnection(object):
             passwd="bubo1234",
             db="gnue_irc",
             use_unicode=True,
-            charset="utf8"
+            charset="utf8",
+            cursorclass=cursors.SSCursor
         )
         self.cursor = self.db.cursor()
         self.cursor.execute('SET NAMES utf8mb4')
--- mark_summary.py
+++ mark_summary.py
@@ -2,6 +2,8 @@ import MySQLdb
 
 from database import DatabaseConnection
 
+connection = DatabaseConnection()
+db, cursor = connection.db, connection.cursor
 
 def get_quotes_for_date(quote_date):
     """
@@ -10,8 +12,6 @@ def get_quotes_for_date(quote_date):
     :return: A hash set containing all quotes for a particular date
     """
     quotes_set = set()
-    connection = DatabaseConnection()
-    db, cursor = connection.db, connection.cursor
 
     try:
         cursor.execute(
@@ -20,20 +20,87 @@ def get_quotes_for_date(quote_date):
         )
         para_quotes = cursor.fetchall()
 
-        for quote in para_quotes:
-            quotes = quote[0].split('-')
+        for (quote,) in para_quotes:
+            quotes = quote.split('-')
             for q in quotes:
-                q = q.rstrip()
+                q = q.strip()
                 q = q.replace('\n', '')
+                q = ' '.join(q.split())
                 quotes_set.add(q)
 
-        print(quotes_set)
+        # print(quotes_set)
+
+    except MySQLdb.Error as error:
+        print(error)
+        db.rollback()
+
+    return quotes_set
+
+
+# get_quotes_for_date("2002-04-10")
+
+def get_log_ids_of_quoted_logs(date_of_log):
+    quotes_set = get_quotes_for_date(date_of_log)
+    quoted_log_ids = []
+
+    try:
+        cursor.execute(
+            u"SELECT log_id, line_message FROM GNUeIRCLogs "
+            u"WHERE date_of_log=%s", (date_of_log,)
+        )
+        for log_id, line_message in cursor.fetchall():
+            line_message = line_message.strip()
+            line_message = line_message.replace('\n', '')
+            line_message = ' '.join(line_message.split())
+            if line_message in quotes_set:
+                quoted_log_ids.append(int(log_id))
+                # print log_id, line_message
+
+    except MySQLdb.Error as error:
+        print(error)
+        db.rollback()
+    return quoted_log_ids
+
+
+def mark_quoted_logs_as_summary_per_date(date_of_log):
+    log_ids = get_log_ids_of_quoted_logs(date_of_log)
+    format_ids = ','.join(['%s'] * len(log_ids))
+    log_ids.append(date_of_log)
+    print date_of_log
+    if len(log_ids) > 1:
+        try:
+            cursor.execute(
+                u"UPDATE GNUeIRCLogs SET is_summary=1 "
+                u"WHERE log_id IN ({}) "
+                u"AND date_of_log=%s".format(format_ids), tuple(log_ids)
+            )
+
+        except MySQLdb.Error as error:
+            print(error)
+    else:
+        print "No quotes found for: ", date_of_log
+
+
+def mark_all_quoted_logs_as_summary():
+    try:
+        cursor.execute(
+            u"SELECT date_of_log FROM GNUeIRCLogs "
+            u"GROUP BY date_of_log ORDER BY date_of_log"
+        )
+        # print list(cursor)
+        for (date_of_log,) in cursor.fetchall():
+            if "2001-10-23" <= date_of_log <= "2006-09-21":
+                # print date_of_log
+                print 'Processing Log: ', date_of_log
+                mark_quoted_logs_as_summary_per_date(date_of_log)
 
     except MySQLdb.Error as error:
         print(error)
         db.rollback()
 
 
-# get_quotes_for_date("2002-04-10")
+mark_all_quoted_logs_as_summary()
+# mark_quoted_logs_as_summary_per_date("2001-10-23")
 
-
+# disconnect from server
+db.close()
