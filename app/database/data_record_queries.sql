66720c892f01ceea6172dfa0e8d5562e4744a06d
Cynthia Wasonga <wasonga.cynthia@gmail.com>
2019-02-12 17:08:32 +0300

    Generate summarized sentiments and word count files

---
+++ app/database/data_record_queries.sql
@@ -0,0 +1,19 @@
+-- Select only logs with summaries (ones I have gone through
+SELECT COUNT(*) FROM GNUeIRCLogs
+WHERE date_of_log >= '2001-10-23' AND date_of_log <= '2001-11-18';
+-- 578572 TOTAL Summarized
+-- 20715 currently processed
+
+SELECT log_id, line_message, date_of_log FROM GNUeIRCLogs
+WHERE date_of_log >= '2001-10-23' AND date_of_log <= '2001-11-18'
+ORDER BY log_id;
+
+
+SELECT MIN(gnu.log_id) AS min_log_id, gnu.date_of_log, COUNT(gnu.log_id) as chat_line_count
+FROM (SELECT log_id, date_of_log FROM GNUeIRCLogs) AS gnu
+WHERE date_of_log >= '2001-10-23' AND date_of_log <= '2001-11-18'
+GROUP BY gnu.date_of_log
+ORDER BY MIN(gnu.log_id) ASC
+;
+
+
